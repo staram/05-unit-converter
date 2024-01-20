d73c9a76b0949b40c64c6165a4e4a0d3210d6e63
staram <10952725+staram@users.noreply.github.com>
2024-01-20 00:30:50 +0000

    Add JS elements
    Add JS calculations to file

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+let convertButton = document.getElementById("input-btn")
+let inputEl = document.getElementById("input-value")
+
+let inputNumber = document.getElementById("input-number")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+
+const meterToFeet = 3.28084
+const literToGallon = 0.264172
+const kilogramToPound = 2.20462
+
+convertButton.addEventListener("click", function() { 
+    let baseValue = inputEl.value
+    let length1 = baseValue * meterToFeet
+    let length2 = baseValue * 1/meterToFeet
+    let volume1 = baseValue * literToGallon
+    let volume2 = baseValue * 1/literToGallon
+    let mass1 = baseValue * kilogramToPound
+    let mass2 = baseValue * 1/kilogramToPound
+
+    lengthEl.textContent = `${baseValue} m = ${length1.toFixed(3)} ft | ${baseValue} ft = ${length2.toFixed(3)} m`
+
+    volumeEl.textContent = `${baseValue} L = ${volume1.toFixed(3)} US gal | ${baseValue} US gal = ${volume2.toFixed(3)} L`
+
+    massEl.textContent = `${baseValue} kg = ${mass1.toFixed(3)} lb | ${baseValue} lb = ${mass2.toFixed(3)} kg`
+})
